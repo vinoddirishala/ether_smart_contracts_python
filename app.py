329b6f124559bc4865ba1870d7e5abeae60d7621
Vinod Dirishala <22494324+vinoddirishala@users.noreply.github.com>
2021-11-16 15:12:19 +0530

    django dapp work started

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ import json
 from dotenv import load_dotenv
 import string
 
+
+
 load_dotenv()
 
 #generate private key and wallet address
@@ -16,10 +18,11 @@ privateKey = "0x"+privateKeyWithOutPrefix
 walletAddress = Account.from_key(privateKey)
 
 
-#privateKey = "b7d578d85714801883979093479417801a9c15a682c8eb5cf7e556a9cbb54ed8"
-#walletAddress = "0x2bB82C830Efe707C864b5bfEad7e5610459AB240"
+#fromPrivateKey = "b7d578d85714801883979093479417801a9c15a682c8eb5cf7e556a9cbb54ed8"
+#fromWalletAddress = "0x2bB82C830Efe707C864b5bfEad7e5610459AB240"
 
-
+privateKey = "5f8970adc9d9fe3d8a8981a848ee7c4bff9fd085d6977f4b4ea7b5cd27da7584"
+walletAddress = "0x4E140256a24E42396dF7953Ce76067F97d906788"
 
 #load data from env file
 rpcURL = os.environ['RPC_URL']
@@ -31,16 +34,34 @@ web3 = Web3(Web3.HTTPProvider(rpcURL))
 
 
 if web3.isConnected():
+
+    #This code is useful to transfer funds from one wallet to other wallet address
+    #transactionBody = {
+    #    'nonce':web3.eth.getTransactionCount(fromWalletAddress),
+    #    'to':walletAddress.address,
+    #    'value':web3.toWei(20,'ether'),
+    #    'gas':2000000,
+    #    'gasPrice':web3.toWei('50','gwei')
+    #}
+
+    #signTransaction = web3.eth.account.sign_transaction(transactionBody,fromPrivateKey) 
+    #txnHash = web3.eth.sendRawTransaction(signTransaction.rawTransaction)
+    #print(web3.toHex(txnHash))
+
+
     guess_number = web3.eth.contract(abi=abiJson,bytecode=byteCode)
+    print(web3.eth.getTransactionCount(walletAddress))
     transactionBody = {
-        'nonce':web3.eth.getTransactionCount(walletAddress.address),
-        'value':web3.toWei(0.1,'ether'),
+       # 'nonce':web3.eth.getTransactionCount(walletAddress.address),
+        'nonce':web3.eth.getTransactionCount(walletAddress),
+        'value':web3.toWei(0.00001,'ether'),
         'gas':2000000,
-        'gasPrice':web3.toWei('50','gwei')
+        'gasPrice':web3.toWei('0.00005','gwei')
     }
     signTransaction = web3.eth.account.sign_transaction(transactionBody,privateKey) 
     txnHash = web3.eth.sendRawTransaction(signTransaction.rawTransaction)
     print(web3.toHex(txnHash))
+    
 
 
     
--- app_copy.py
+++ app_copy.py
@@ -1,52 +1,65 @@
 from eth_account import Account
+from eth_account.signers.local import LocalAccount
 from eth_typing import abi
 from web3 import Web3
 import secrets
 import os
 import json
 from dotenv import load_dotenv
+import string
 
 load_dotenv()
 
-priv = secrets.token_hex(32)
-private_key = "0x"+priv
-#print("PrivateKey of your wallet:::"+private_key)
-acc = Account.from_key(private_key)
-#print("Your Public Wallet Address of Etherium:::"+acc.address)
+#generate private key and wallet address
+privateKeyWithOutPrefix = secrets.token_hex(32)
+privateKey = "0x"+privateKeyWithOutPrefix
+walletAddress = Account.from_key(privateKey)
 
 
-abi = {}
+fromPrivateKey = "b7d578d85714801883979093479417801a9c15a682c8eb5cf7e556a9cbb54ed8"
+fromWalletAddress = "0x2bB82C830Efe707C864b5bfEad7e5610459AB240"
 
 
-web3 = Web3(Web3.HTTPProvider(os.environ['RPC_URL']))
+
+#load data from env file
+rpcURL = os.environ['RPC_URL']
+abiJson = json.loads(os.environ['CONTRACT_ABI'])
+byteCode = os.environ['CONTRACT_BYTE_CDOE']
+
+web3 = Web3(Web3.HTTPProvider(rpcURL))
+
+print(privateKey+"----"+
+walletAddress.address)
 
 if web3.isConnected():
 
-    account1 = "0x2bB82C830Efe707C864b5bfEad7e5610459AB240"
-    account2 = "0xd8bFc5271635C6EbBaAEab564A734F478dB52850"
-    privateKey = "b7d578d85714801883979093479417801a9c15a682c8eb5cf7e556a9cbb54ed8"
+    #This code is useful to transfer funds from one wallet to other wallet address
+    #transactionBody = {
+    #    'nonce':web3.eth.getTransactionCount(fromWalletAddress),
+    #    'to':walletAddress.address,
+    #   'value':web3.toWei(20,'ether'),
+    #    'gas':2000000,
+    #    'gasPrice':web3.toWei('50','gwei')
+    #}
 
-    nonce = web3.eth.getTransactionCount(account1)
+    #signTransaction = web3.eth.account.sign_transaction(transactionBody,fromPrivateKey) 
+    #txnHash = web3.eth.sendRawTransaction(signTransaction.rawTransaction)
+    #print(web3.toHex(txnHash))
 
 
-    tx = {
-        'nonce':nonce,
-        'to':account2,
-        'value':web3.toWei(0.00001,'ether'),
+    guess_number = web3.eth.contract(abi=abiJson,bytecode=byteCode)
+    transactionBody = {
+        'nonce':web3.eth.getTransactionCount(walletAddress.address),
+        'value':web3.toWei(0.1,'ether'),
         'gas':2000000,
         'gasPrice':web3.toWei('50','gwei')
     }
-
-    signedTX = web3.eth.account.signTransaction(tx,privateKey)
-    txHash = web3.eth.sendRawTransaction(signedTX.rawTransaction)
-    print(web3.toHex(txHash))
-
-
-    web3.eth.contract(address=account1,abi=abi)
+    signTransaction = web3.eth.account.sign_transaction(transactionBody,privateKey) 
+    txnHash = web3.eth.sendRawTransaction(signTransaction.rawTransaction)
+    print(web3.toHex(txnHash))
 
 
-
-
+    
 else:
     print("Connect to wallet to continue")
 
