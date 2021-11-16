c7044814176085868696f7f3550fa5afbdc15251
Vinod Dirishala <22494324+vinoddirishala@users.noreply.github.com>
2021-11-16 13:42:54 +0530

    smart contract deployment code added

--- app.py
+++ app.py
@@ -1,41 +1,49 @@
 from eth_account import Account
+from eth_account.signers.local import LocalAccount
+from eth_typing import abi
 from web3 import Web3
 import secrets
+import os
+import json
+from dotenv import load_dotenv
+import string
 
-priv = secrets.token_hex(32)
-private_key = "0x"+priv
-#print("PrivateKey of your wallet:::"+private_key)
-acc = Account.from_key(private_key)
-#print("Your Public Wallet Address of Etherium:::"+acc.address)
+load_dotenv()
 
-rpcURL = "HTTP://127.0.0.1:7545"
+#generate private key and wallet address
+privateKeyWithOutPrefix = secrets.token_hex(32)
+privateKey = "0x"+privateKeyWithOutPrefix
+walletAddress = Account.from_key(privateKey)
+
+
+#privateKey = "b7d578d85714801883979093479417801a9c15a682c8eb5cf7e556a9cbb54ed8"
+#walletAddress = "0x2bB82C830Efe707C864b5bfEad7e5610459AB240"
+
+
+
+#load data from env file
+rpcURL = os.environ['RPC_URL']
+abiJson = json.loads(os.environ['CONTRACT_ABI'])
+byteCode = os.environ['CONTRACT_BYTE_CDOE']
 
 web3 = Web3(Web3.HTTPProvider(rpcURL))
 
+
+
 if web3.isConnected():
-
-    account1 = "0x2bB82C830Efe707C864b5bfEad7e5610459AB240"
-    account2 = "0xd8bFc5271635C6EbBaAEab564A734F478dB52850"
-    privateKey = "b7d578d85714801883979093479417801a9c15a682c8eb5cf7e556a9cbb54ed8"
-
-    nonce = web3.eth.getTransactionCount(account1)
-    print(web3.toInt(web3.eth.get_balance(account1)))
-
-
-    tx = {
-        'nonce':nonce,
-        'to':account2,
-        'value':web3.toWei(0.00001,'ether'),
+    guess_number = web3.eth.contract(abi=abiJson,bytecode=byteCode)
+    transactionBody = {
+        'nonce':web3.eth.getTransactionCount(walletAddress.address),
+        'value':web3.toWei(0.1,'ether'),
         'gas':2000000,
         'gasPrice':web3.toWei('50','gwei')
     }
+    signTransaction = web3.eth.account.sign_transaction(transactionBody,privateKey) 
+    txnHash = web3.eth.sendRawTransaction(signTransaction.rawTransaction)
+    print(web3.toHex(txnHash))
 
-    signedTX = web3.eth.account.signTransaction(tx,privateKey)
-    txHash = web3.eth.sendRawTransaction(signedTX.rawTransaction)
-    print(web3.toHex(txHash))
 
-    print(web3.toInt(web3.eth.get_balance(account1)))
-
+    
 else:
     print("Connect to wallet to continue")
 
